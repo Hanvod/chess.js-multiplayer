2f918b66d881150f1455b8c339c2d55a7e11a9ab
Hanvod <hanvodv2@gmail.com>
2021-01-22 14:32:08 +0300

    Revert "Revert "Update gameServer.ts""
    This reverts commit f566f299e574a3e1590713fe5f489dbb44c21323.

--- server/src/gameServer.ts
+++ server/src/gameServer.ts
@@ -1,7 +1,7 @@
 import { Socket } from "socket.io";
 import ChessInstanceWrapper from "./chessWrapperBase";
 import GamePermissions from "./userPermissions";
-import { Move, ShortMove } from "chess.js"
+import { Move, ShortMove, Square, Piece } from "chess.js"
 
 class GameServerClient {
     public readonly socket: Socket;
@@ -21,59 +21,42 @@ class GameServerClient {
 class GameServer extends ChessInstanceWrapper {
     public users: GameServerClient[]
 
+    // --------------------------------------
+    //               Network
+    // --------------------------------------
+
     private resync(client: GameServerClient) {
         client.socket.emit("chess::resync", this.instance.fen())
     }
-
-    private broadcastMethodCall(method: string, args: any[]) {
-        this.users.forEach(client => client.socket.emit("chess::method_call", method, args))
-    }
-
-    private methodCallHandler(client: GameServerClient, method: string, args: any[], respond: (boolean) => void) {
-        const permissions = client.permissions
-        let success = false
-
-        if(permissions.methods.includes(method)) {
-            try {
-                this[method]()
-                success = true
-            }
-            catch(err) {
-                success = false
-            }
-        }
-        else if(method === "move") {
-            // Handle move by game rules
-            return this.moveHandler(client, args[0], respond)
-        }
-
-        respond(success)
-    }
     
-    private moveHandler(client: GameServerClient, move: ShortMove | Move | string, respond: (boolean) => void) {
-        const permissions = client.permissions
+    /**
+     * 
+     * @param method Имя метода
+     * @param args Массив аргументов
+     * @param ignoredClient Не передавать информацию этому клиенту (используется, когда метод вызван самим клиентом)
+     */
+    private sharedMethodCall(method: string, args: any[], ignoredClient?: GameServerClient): any | Error {
+        let result: any = null
         
-        const allowedToMakeInGameMove = (permissions.canPlayBlack && this.turn() === "b") || (permissions.canPlayWhite && this.turn() === "w")
-        
-        let success = true
-
-        if(allowedToMakeInGameMove) {
-            try {
-                this.move(move)
-                success = true
-            }
-            catch(err) {
-                success = false
-            }
+        try {
+            result = this[method](...args)
+        }
+        catch(err) {
+            return new Error()
         }
 
-        respond(allowedToMakeInGameMove && success)
-    }
+        this.users.forEach(client => {
+            if(!ignoredClient || client !== ignoredClient) {
+                client.socket.emit("chess::method_call", method, args)
+            }
+        })
 
-    private addEventHandlers(client: GameServerClient) {
-        client.socket.on("chess::method_call", (method: string, args: any[]) => this.methodCallHandler(client, method, args))
-        client.socket.on("chess::move", (move: ShortMove | Move | string, respond: (boolean) => void) => this.moveHandler(client, move, respond))
-    }
+        return result
+    }    
+
+    // --------------------------------------
+    //          Client management
+    // --------------------------------------
 
     /**
      * Adds client and instantly synchronize board
@@ -88,5 +71,78 @@ class GameServer extends ChessInstanceWrapper {
         return client
     }
 
+    private addEventHandlers(client: GameServerClient) {
+        client.socket.on("chess::method_call", (method: string, args: any[], respond: (boolean) => void) => this.methodCallHandler(client, method, args, respond))
+    }
+
+    private methodCallHandler(client: GameServerClient, method: string, args: any[], respond: (boolean) => void) {
+        let success = false
+
+        if(client.permissions.methods.includes(method)) {
+            success = !(this.sharedMethodCall(method, args, client) instanceof Error)
+        }
+        // Try to make move by game rules
+        else if(method === "move") {
+            success = this.tryToMoveByRules(client, args[0])
+        }
+
+        respond(success)
+    }
     
+    private tryToMoveByRules(client: GameServerClient, move: ShortMove | Move | string) {
+        let success = false
+        
+        const canPlayBlack = client.permissions.canPlayBlack
+        const canPlayWhite = client.permissions.canPlayWhite
+        
+        const allowedToMakeInGameMove = (canPlayBlack && this.turn() === "b") || (canPlayWhite && this.turn() === "w")
+        
+        if(allowedToMakeInGameMove) {
+            success = !(this.sharedMethodCall("move", [move], client) instanceof Error)
+        }
+
+        return success
+    }
+  
+    // --------------------------------------
+    //       Synchronized Board API
+    // --------------------------------------
+
+    //"!undo", "!reset", "!remove", "!put", "!move", "!load_pgn", "!load", "clear"
+
+    public move(move: string | ShortMove,
+            options?: {
+            sloppy?: boolean;
+        },
+    ): Move | null {
+        return this.sharedMethodCall("move", [move])
+    }
+    
+    public undo(): Move {
+        return this.sharedMethodCall("undo", [])
+    }
+
+    public reset(): void {
+        return this.sharedMethodCall("reset", [])
+    }
+
+    public remove(square: Square): Piece {
+        return this.sharedMethodCall("remove", [square])
+    }
+
+    public put(piece: Piece, square: Square): boolean  {
+        return this.sharedMethodCall("put", [piece, square])
+    }
+
+    public load_pgn(pgn: string, options?: { newline_char?: string; sloppy?: boolean; }): boolean {
+        return this.sharedMethodCall("load_pgn", [pgn, options])
+    }
+
+    public load(fen: string): boolean {
+        return this.sharedMethodCall("load", [fen])
+    }
+
+    public clear(): void {
+        return this.sharedMethodCall("clear", [])
+    }
 }
